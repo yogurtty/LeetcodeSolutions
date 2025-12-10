fcb55275245bb544293d6b2ca4a2a8b52ce18ab6
yogurtty <jappiahhodges@gmail.com>
2025-12-09 22:20:50 -0500

    did basic binary search then attempted a hard problem and didnt finish

---
+++ JavaBeLike/BinarySearch.java
@@ -0,0 +1,26 @@
+public class BinarySearch {
+
+    public static void main(String[] args) {
+    int[] nums = new int[]{7,8,4,5,3,9,10,32,16,88,67,1,0,2};
+    int target = 67;
+    System.out.println(binarySearch(nums, target));
+        System.out.println("target is " + target + " and we found " + nums[binarySearch(nums,target)] + " at index " + binarySearch(nums,target) );
+    }
+    public static int binarySearch(int[] nums, int target){
+        int low = 0;
+        int high = nums.length-1;
+        while (low <= high) {
+            int mid = (low + high)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                low = mid+1;
+            }
+            else if(nums[mid] > target){
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# LeetcodeSolutions
-Where I work on Leetcode problems because i can use more in depth debugging tools than in the browser to understand the problem.
+# LeetCodeSolutions
+Where I work on LeetCode problems because i can use more in depth debugging tools than in the browser to understand the problem.
 Mostly in java but may include other language solutions as I learn them
